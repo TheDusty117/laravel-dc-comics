f65ac12a2b54aa41d84d879846c7d36d0ed54ca2
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-14 16:10:15 +0200

    FEAT foreach of seeder

---
+++ database/seeders/ComicsSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Comic;
+
+class ComicsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+        foreach ($variable as $value) {
+            $newComic = new Comic();
+
+            $newComic->
+
+        }
+
+
+    }
+}
