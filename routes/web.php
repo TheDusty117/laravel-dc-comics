bf61d6a65693a010c3880383a11ff710ce69a3bc
TheDusty117 <gabrieledipassio@gmail.com>
2023-04-14 17:16:36 +0200

    FEAT trying index show create

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/comics',[ComicController::class, 'index']);
+
+
+Route::get('/comics',[ComicController::class, 'index'])->name('comics.index');
+
+Route::get('/comics/create',[ComicController::class, 'create'])->name('comics.create');
+
+Route::get('/comics/{comic}',[ComicController::class, 'show'])->name('comics.show');
+
+
